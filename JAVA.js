ccbb65123e27456abfa2f44d4444196dc713cb6e
DAXDAY <90981062+DAXDAY@users.noreply.github.com>
2021-11-25 20:47:36 -0300

    site-final

---
+++ JAVA.js
@@ -0,0 +1,25 @@
+let seu_nome=prompt('digite aqui o seu nome')
+let seu_idae=prompt('digite aqui a sua idade')
+if(seu_nome=seu_nome+seu_idae){}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ efeitos/JAVA.js
@@ -0,0 +1,25 @@
+let seu_nome=prompt('digite aqui o seu nome')
+let seu_idae=prompt('digite aqui a sua idade')
+<input type="number" value ="10">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
